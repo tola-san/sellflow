07e01ec5334e7d90b4fe5e6270e995904982a72a
Tola SAN <stola@cellcard.com.kh>
2026-07-17 23:00:12 +0700

    fix and debug upload image broken

--- sellflow-api/config/services.php
+++ sellflow-api/config/services.php
@@ -22,6 +22,10 @@ return [
         'key' => env('RESEND_API_KEY'),
     ],
 
+    'cloudinary' => [
+        'url' => env('CLOUDINARY_URL'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
